2e00fbd2df2f646438367fd1d94d81da7029e6cd
lokesh <lokeshwar5221@gmail.com>
2026-06-23 13:28:53 +0530

    Add files via upload

---
+++ INTRODUCTION D1.py
@@ -0,0 +1,136 @@
+'''
+PROCEDURAL LANGUAGE
+------------------
+--> This follows a step by step approach,where code is structure into procedure such as
+fuction or routines...  but python line by line execution
+eg --> c
+
+
+OBJECT ORIENTED
+---------------
+-->It based on concepts of object and class...
+--> #Example
+'''
+print(89+67) # BUt output is came it runtime it will be used
+num = 89
+num_2 = 67
+total_ = num + num_2
+print(id(num))
+print(id(num_2))
+print(id(total_))
+print (type(num))
+print(id(num))
+# But It can be save and store and acceess the data by using id
+#If any create object that can be store permentaly
+
+
+'''
+WHAT IS PYTHON
+--------------
+1.Dynamically typed
+--------------------
+-->python Knows the type of data we passing to the variable...
+'''
+num=10
+print(type(num))
+'''
+2.Interperter
+--------------
+-->Python Excute code line-by-line,if any error occurs it will stop the execution in that line and gives output for before lines of code...
+'''
+num = 89
+num_2 = 67
+total_ = num + num_2
+print(id(num))
+print(id(num_2))
+print(id(total_))
+print(total)
+print (type(num))
+'''       
+3.High_level
+-------------
+-->It will be the taken low memory allocation we can not take, it will take high level
+
+-->Python In Year 1991,Guide Van Russum
+
+
+
+
+
+
+WHY TO USE PYTHON
+-----------------
+--> easy syntax
+-->cross-platform:Wide platforms like node++,nodepad,linux,Windows
+-->wide application:Use in data science,Ai,Machine learning
+-->Huge library support:Pandas,Matplot lib
+
+
+
+TOKENS
+------
+These words are reserved
+-->We have 5 Tokens
+
+1.Keyword
+---------
+-->these are reserved words in python
+Example-->If,else,for,while
+
+2.Identifires
+-------------
+-->names given to varibles,functions,values
+
+3.Literals
+-->It is nothing but Data Types,Right side of Literals
+Example so ="hi", hi is called literals
+
+4,Operators
+------------
+-->+,-,*,/
+
+5.Punctuators
+-------------
+-->Square,Curly,partences
+{},[],()
+
+
+
+
+
+Variables
+---------
+-->The value that can be store in data
+Rules of Variable
+
+#Good
+-->It can be start with small letter,captial letter
+eg:
+num_1=7
+num=8
+Num_2 =9
+
+#Bad
+-->It should not start with Special char,number,space
+$num =0
+su@m = 8
+s o = 89
+2num =9
+
+
+
+Comments
+--------
+1.Single line ---> #
+2.Multi line ---> ''' ''',""" """
+
+
+
+Boolean
+-------
+-->It tells about true or false
+-->Example
+'''
+a=12
+b=23
+print(a==b)
