4d1db0dd581c72e71d0a8461f1f89c546404ae6f
lokesh <lokeshwar5221@gmail.com>
2026-06-30 12:07:39 +0530

    Add files via upload

---
+++ Input formatting D7.py
@@ -0,0 +1,51 @@
+'''
+Input Formatting from user
+input()
+-------
+the input() function is used to take input from the user...
+
+1.int
+-----
+Example
+'''
+num=int(input("enter a number:"))
+num2=int(input("enter a number:"))
+print(num+num2)
+
+'''         
+2.string
+--------
+Example
+'''
+how=input("enter char:")
+when=how+"3"
+print(when)
+
+'''
+3.float
+-------
+Example
+'''
+num=float(input("enter your salary"))
+print("your salary is:",num)
+
+'''
+4.list
+------
+Example
+'''
+group_=list(map(int,input().split()))
+group1=list(input())
+print(group2)
+print(group_)
+
+'''
+5.tuple
+-------
+Example
+'''
+some=tuple(map(int,input().split()))
+print(some)
+
+group_=tuple(input().split())
+print(group_)
